a45a9eb8bc7e8d73a413c2efd0eb43236af2902f
seong-yeop <ucassje@ucl.ac.uk>
2021-05-31 11:24:07 +0100

    update

--- JSY5/EulerCN.py
+++ JSY5/EulerCN.py
@@ -18,7 +18,7 @@ from lmfit import Parameters, fit_report, minimize
 #from lmfit import Model
 import lmfit
 
-Nv=61 #velocity step number
+Nv=31 #velocity step number
 i_solar_r=5 #10
 f_solar_r=20 #30
 path_home="/Users/user/Desktop/JSY5/"
@@ -325,7 +325,7 @@ def H_perp(a,b,r):
 
 
 def rect_v(x):
-	return 0 if abs(x)>=abs(pal_v[0]) else 1
+	return 1#0 if abs(x)>=abs(pal_v[0]) else 1
 
 def sin(x):
         return (1/(1+(U_solar(x)/Omega*(1/x))**2)**0.5)
@@ -602,8 +602,8 @@ solu1=np.zeros(shape = (Nv, Nv))
 solu2=np.zeros(shape = (Nv))
 
 
-timestep=1000 #700
-Normvalue=np.zeros(shape = (timestep))
+timestep=500 #700
+Normvalue=np.zeros(shape = (timestep,Nr))
 Normvalue_bulk=np.zeros(shape = (timestep))
 for k in range(timestep):
         print(k)
@@ -829,8 +829,9 @@ for k in range(timestep):
                 for J in range(Nv):
                         for I in range(Nv):
                                 norm=norm+abs((f_next[J*Nv+I,R]/np.max(f_next[:,R])-f_pre[J*Nv+I,R]/np.max(f_pre[:,R])))**2
-        Normvalue[k]=norm**0.5
-        print(norm**0.5)
+                Normvalue[k,R]=norm**0.5
+        #Normvalue[k]=norm**0.5
+        #print(norm**0.5)
 
 np.save('data_next.npy', f_1)          
 
@@ -847,20 +848,23 @@ cont_lev = np.linspace(-10,0,25)
 
 o=np.linspace(1, timestep, timestep)
 
-plt.figure(figsize=(20,15))
-plt.grid()
-ax = plt.gca()
-plt.rc('font', size=35)
-plt.tick_params(labelsize=40)
-plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-ax.set_xlim([o[0],o[timestep-1]])
-ax.set_ylim([10**(-5),10**(-2)])
-ax.set_xlabel(r'$t$', fontsize=28)
-ax.set_ylabel(r'$norm$', fontsize=28)
-ax.plot(o,Normvalue,linewidth=3.0, color='k');
-plt.savefig(f'{path_current}figure/norm.png')
-plt.clf()
-plt.close()
+for r in range(Nr):
+        plt.figure(figsize=(20,15))
+        plt.grid()
+        ax = plt.gca()
+        plt.rc('font', size=35)
+        plt.tick_params(labelsize=40)
+        plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        ax.set_xlim([o[0],o[timestep-1]])
+        ax.set_ylim([10**(-5),10**(-2)])
+        ax.set_xlabel(r'$t$', fontsize=28)
+        ax.set_ylabel(r'$norm$', fontsize=28)
+        ax.plot(o,Normvalue[:,r],linewidth=3.0, color='k');
+        plt.savefig(f'{path_current}norm/{r}.png')
+        plt.clf()
+        plt.close()
+
+
 
 
 for r in range(Nr):
